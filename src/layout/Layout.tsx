7eca9fe0101b96c7ec0a2bc55d94ea09271a8f9b
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-14 11:11:13 -0300

    add Layout component with theme toggle and background styling

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { useStoreTheme, ToogleTheme } from "../components/ToogleTheme";
+import { BG_DARK_THEME, BG_LIGHT_THEME } from "../const";
+import { Outlet } from "react-router";
+
+export default function Layout() {
+	const theme = useStoreTheme((state) => state.theme);
+
+	const bg = theme === "light" ? BG_DARK_THEME : BG_LIGHT_THEME;
+	const colorText = theme === "light" ? "text-white" : "text-black";
+
+	return (
+		<section className={`${colorText} h-full [&_main]:h-full`}>
+			<header>
+				<ToogleTheme classList="absolute top-4 right-4" />
+			</header>
+			<Outlet />
+			<div className={`${bg}`} />
+		</section>
+	);
+}
