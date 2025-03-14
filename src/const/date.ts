38859cd60a52a95f1780b8fd0419ec4fe68e864b
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-14 11:12:37 -0300

    add constants for days and months in date utility

---
+++ src/const/date.ts
@@ -0,0 +1,4 @@
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+export {DAYS, MONTHS}
