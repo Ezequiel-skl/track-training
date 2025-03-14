e65a40642b8d05fbd13868af0ab7ba0e8aae3456
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-14 11:12:11 -0300

    add main App component with routing and dynamic favicon support

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import Layout from "./layout/Layout";
+import Home from "./page/Home";
+import TrainingDays from "./page/TrainingDays";
+
+import { Route, Routes, BrowserRouter, useNavigate } from "react-router";
+import dynamicFavicon from "./utils/dynamicFavicon";
+
+function CheckTrackDay() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const daysStorage = localStorage.getItem("day");
+		if (daysStorage) {
+			navigate("/home");
+		}
+	}, [navigate]);
+
+	return <TrainingDays />;
+}
+
+function App() {
+	dynamicFavicon();
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="/" element={<CheckTrackDay />} />
+					<Route path="/home" element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
