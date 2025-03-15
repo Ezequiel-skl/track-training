b6b59d9fb46ffb208dbf2c276566ccef82a8abe3
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-15 02:00:42 -0300

    enhance Home component with styled button and integrate CalendarMonth

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,21 @@
+import CalendarMonth from "@/components/CalendarMonth";
+
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-center">
 			<div className="w-fit text-center">
 				<header>
-					<button type="button">check days</button>
+					<button
+						type="button"
+						className="
+              text-4xl font-semibold border-2 border-gray-400 rounded-lg px-4 py-2 hover:cursor-pointer hover:opacity-90
+              mb-10 hover:border-green-600 hover:text-green-600
+              "
+					>
+						check days
+					</button>
 				</header>
+				<CalendarMonth />
 			</div>
 		</main>
 	);
