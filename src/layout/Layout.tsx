fe8ee231d6bb385d4192ae8d3e9d5266a6e86828
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-18 09:30:59 -0300

    refactor Layout component to remove ToggleTheme and integrate Header and DevTools

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,20 +1,23 @@
-import { useStoreTheme, ToogleTheme } from "../components/ToggleTheme";
+import { useStoreTheme } from "../components/ToggleTheme";
 import { BG_DARK_THEME, BG_LIGHT_THEME } from "../const/bgTheme";
 import { Outlet } from "react-router";
+import Header from "@/components/Header";
+import DevTools from "@/components/DevTools";
 
 export default function Layout() {
 	const theme = useStoreTheme((state) => state.theme);
 
 	const bg = theme === "light" ? BG_DARK_THEME : BG_LIGHT_THEME;
-	const colorText = theme === "light" ? "text-white" : "text-black";
+	const colorText = theme === "light" ? "darkTheme" : "lightTheme";
 
 	return (
-		<section className={`${colorText} h-full [&_main]:h-full`}>
-			<header>
-				<ToogleTheme classList="absolute top-4 right-4" />
-			</header>
-			<Outlet />
-			<div className={`${bg}`} />
-		</section>
+		<>
+			<section className={`${colorText} h-full [&_main]:h-full`}>
+				<Header />
+				<Outlet />
+				<div className={`${bg}`} />
+			</section>
+			<DevTools />
+		</>
 	);
 }
