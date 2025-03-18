02f388456c026eb80401c072ab36da71f7b4c841
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-18 09:30:22 -0300

    refactor ToggleTheme component to remove classList prop and improve styling

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useStoreTheme } from "../store/themeStore";
 
-function ToogleTheme({ classList }: { classList?: string }) {
+function ToogleTheme() {
 	const { theme, toogleTheme } = useStoreTheme();
 
 	useEffect(() => {
@@ -15,13 +15,26 @@ function ToogleTheme({ classList }: { classList?: string }) {
 			onClick={toogleTheme}
 			type="button"
 			title={title}
-			className={classList}
+			className="
+        p-1 border-2 border-gray-400 rounded-full w-17 h-8
+        flex items-center justify-start
+        hover:cursor-pointer hover:opacity-90
+        transition-colors duration-300
+      "
 		>
-			{theme === "light" ? (
-				<img src="./sun.svg" alt="An icon of a Sun" />
-			) : (
-				<img src="./moon.svg" alt="An icon of a Moon" />
-			)}
+			<div
+				className={`
+          absolute h-6 w-6 rounded-full border-2 border-gray-400 flex items-center justify-center
+          transform transition-transform duration-300
+          ${theme === "light" ? "translate-x-0" : "translate-x-8"}
+        `}
+			>
+				{theme === "light" ? (
+					<img src="./sun.svg" alt="An icon of a Sun" className="w-4 h-4" />
+				) : (
+					<img src="./moon.svg" alt="An icon of a Moon" className="w-4 h-4" />
+				)}
+			</div>
 		</button>
 	);
 }
