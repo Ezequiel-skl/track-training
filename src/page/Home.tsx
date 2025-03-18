f5b133fbe6886b15368975ef26ff544aadf0f8dc
Ezequiel-sk <ezequielsmiaak@gmail.com>
2025-03-18 09:31:57 -0300

    refactor Home component to replace button with CheckDay and integrate Statistics alongside CalendarMonth

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,21 +1,18 @@
 import CalendarMonth from "@/components/CalendarMonth";
+import CheckDay from "@/components/CheckDay";
+import Statistics from "@/components/Statistics";
 
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-center">
 			<div className="w-fit text-center">
 				<header>
-					<button
-						type="button"
-						className="
-              text-4xl font-semibold border-2 border-gray-400 rounded-lg px-4 py-2 hover:cursor-pointer hover:opacity-90
-              mb-10 hover:border-green-600 hover:text-green-600
-              "
-					>
-						check days
-					</button>
+					<CheckDay />
 				</header>
-				<CalendarMonth />
+				<div className="flex gap-5">
+					<CalendarMonth />
+					<Statistics />
+				</div>
 			</div>
 		</main>
 	);
